61eeea2fb7730b9cc7b4c1028135d89694383834
Andrew McNeill <andrewmcneill@gmail.com>
2012-04-01 02:21:28 +0100

    first try at Fermat method for problem 3

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -1,6 +1,6 @@
 # Project Euler
-# 
-# 1. Add all the natural numbers below one thousand that are multiples of 3 or 5
+
+from math import *
 
 class Euler1(object):
     """
@@ -38,13 +38,22 @@ class Euler3(object):
     """
     Find the largest prime factor of a composite number
     """
+    
     def __init__(self, composite):
         self.composite = composite
+    
     def isOdd(self, number):
         if str(number)[-1] in set(['1','3','5','7','9']):
             return True
         else:
             return False
+    
+    def isSquare(self, number):
+        if number < 0:
+            return False
+        else:
+            return sqrt(number).is_integer()
+    
     def solve(self, number):
         if number % 2 == 0:
             i = number / 2
@@ -57,7 +66,22 @@ class Euler3(object):
                 i = i - 1
         return number
         
+    def Fermat(self, number):
+        if self.isOdd(number):
+            n = number
+        else:
+            n = (number / 2) - 1
+        a = ceil(sqrt(n))
+        b2 = (a * a) - n
+        while not self.isSquare(b2):
+            a = a + 1
+            b2 = a * a - n
+        return int(number / (a - sqrt(b2)))
+            
+        
 x = Euler3(600851475143)
-# x = Euler3(23233414)
+# x = Euler3(3576576)
 
-print x.solve(x.composite)
+print 'hi'
+print x.Fermat(x.composite)
+print x.solve(29)
